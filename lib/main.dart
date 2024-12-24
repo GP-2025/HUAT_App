d48ce5bc8bfab29e3ea5dadd4f6cc00a20450050
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-12-24 02:13:55 +0200

    Revert "file management"
    This reverts commit f89bb27f212190ff81f46fcdff00a198cc4aefb7.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:msccolone/features/home/ui/home_screen.dart';
-import 'package:msccolone/features/login/ui/login_screen.dart';
-import 'generated/l10n.dart';
 import 'mscclone_app.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false ,
-    localizationsDelegates: const [
-      S.delegate, // The generated delegate for strings
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: S.delegate.supportedLocales, // Supported locales
-    locale: Locale('en'),
-      home: LoginPage()  ,
+      home: mscClone_App()  ,
   ));
 }
 
