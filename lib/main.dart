40ba98402789c1cfafa11969c1fa022ac2411338
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-12-24 02:29:42 +0200

    Reapply "Reapply "file management""
    This reverts commit 174f8e6c6fb07a02b0c8da38858556614d5e8baf.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:msccolone/features/home/ui/home_screen.dart';
+import 'package:msccolone/features/login/ui/login_screen.dart';
+import 'generated/l10n.dart';
 import 'mscclone_app.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false ,
-      home: mscClone_App()  ,
+    localizationsDelegates: const [
+      S.delegate, // The generated delegate for strings
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: S.delegate.supportedLocales, // Supported locales
+    locale: Locale('en'),
+      home: LoginPage()  ,
   ));
 }
 
