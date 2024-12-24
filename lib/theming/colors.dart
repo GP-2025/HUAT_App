f89bb27f212190ff81f46fcdff00a198cc4aefb7
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-12-24 02:09:52 +0200

    file management

---
+++ lib/theming/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// All of our static constant stuff
+
+
+class colorManager {
+  static const huatBlue = Color(0xff1892b2),
+      huatDarkBlue = Color(0xff074566),
+      kikiBlack = Colors.black ,
+  huatYellow = Color(0xfff8ba3a);
+
+  static const kPrimaryColor = Color(0xFF366CF6);
+  static const kSecondaryColor = Color(0xFFF5F6FC);
+  static const kBgLightColor = Color(0xFFF2F4FC);
+  static const kBgDarkColor = Color(0xFFEBEDFA);
+  static const kBadgeColor = Color(0xFFEE376E);
+  static const kGrayColor = Color(0xFF8793B2);
+  static const kTitleTextColor = Color(0xFF30384D);
+  static const kTextColor = Color(0xFF4D5875);
+}
